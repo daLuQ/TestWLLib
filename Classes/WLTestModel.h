616d36249b8872c25cd39bc207a2e2b241d72e60
LiuZiStrugle <15850581247@163.com>
2018-12-27 17:06:03 +0800

    添加文件

---
+++ Classes/WLTestModel.h
@@ -0,0 +1,15 @@
+//
+//  WLTestModel.h
+//  TestSearch
+//
+//  Created by 王陆 on 2018/12/27.
+//  Copyright © 2018 南京圈圈网络科技. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface WLTestModel : NSObject
+
+@end
+
---
+++ Classes/WLTestModel.m
@@ -0,0 +1,13 @@
+//
+//  WLTestModel.m
+//  TestSearch
+//
+//  Created by 王陆 on 2018/12/27.
+//  Copyright © 2018 南京圈圈网络科技. All rights reserved.
+//
+
+#import "WLTestModel.h"
+
+@implementation WLTestModel
+
+@end
